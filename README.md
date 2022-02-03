08f05261f3c863e3021dd18086ecd2279f5d83d2
haluskua <haluskua.one@gmail.com>
2022-02-03 16:05:27 +1300

    added allignment styles for header

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Personal Portfolio
 
+# Using Next Js and Styled Components
+
 ![Portfolio Website](https://i.ibb.co/WgPMpts/image.png)
--- src/components/Header/HeaderStyles.js
+++ src/components/Header/HeaderStyles.js
@@ -1,5 +1,5 @@
-import { IoIosArrowDropdown } from 'react-icons/io';
-import styled from 'styled-components';
+import { IoIosArrowDropdown } from "react-icons/io";
+import styled from "styled-components";
 
 export const Container = styled.div`
   display: grid;
@@ -22,9 +22,15 @@ export const Div1 = styled.div`
   display: flex;
   flex-direction: row;
   align-content: center;
+  justify-content: space-around;
   @media ${(props) => props.theme.breakpoints.sm} {
     grid-area: 1 / 1 / 2 / 3;
   }
+`;
+
+export const Span = styled.span`
+  font-size: 2rem;
+  color: orange;
 `;
 export const Div2 = styled.div`
   grid-area: 1 / 2 / 2 / 4;
@@ -94,8 +100,8 @@ export const NavProductsIcon = styled(IoIosArrowDropdown)`
   display: flex;
   align-self: center;
   transition: 0.3s ease;
-  opacity: ${({ isOpen }) => (isOpen ? '1' : '.75')};
-  transform: ${({ isOpen }) => (isOpen ? 'scaleY(-1)' : 'scaleY(1)')};
+  opacity: ${({ isOpen }) => (isOpen ? "1" : ".75")};
+  transform: ${({ isOpen }) => (isOpen ? "scaleY(-1)" : "scaleY(1)")};
 
   &:hover {
     opacity: 1;
@@ -107,18 +113,16 @@ export const NavProductsIcon = styled(IoIosArrowDropdown)`
   }
 `;
 
-
-// Social Icons 
+// Social Icons
 
 export const SocialIcons = styled.a`
-transition: 0.3s ease;
-color: white;
-border-radius: 50px;
+  transition: 0.3s ease;
+  color: white;
+  border-radius: 50px;
   padding: 8px;
-&:hover {
+  &:hover {
     background-color: #212d45;
     transform: scale(1.2);
     cursor: pointer;
-    
   }
-`
+`;
