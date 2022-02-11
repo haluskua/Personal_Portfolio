6440d7555704d5701e54ffe5062e2e22d936eb72
haluskua <haluskua.one@gmail.com>
2022-02-11 15:28:24 +1300

    added framer-motion for title animation

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { motion } from "framer-motion";
 import {
   Section,
   SectionText,
@@ -13,10 +13,28 @@ const Hero = () => (
   <Section row nopadding>
     <LeftSection>
       <BcgArt />
-      <SectionTitle main center>
-        Welcome To <br />
-        My Personal Portfolio
-      </SectionTitle>
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        variants={{
+          hidden: {
+            scale: 0.8,
+            opacity: 0,
+          },
+          visible: {
+            scale: 1,
+            opacity: 1,
+            transition: {
+              delay: 0.4,
+            },
+          },
+        }}
+      >
+        <SectionTitle main center>
+          Welcome To <br />
+          My Personal Portfolio
+        </SectionTitle>
+      </motion.div>
       <SectionText>
         The purpose of this portfolio is to credit my constant pursuit as a
         fullstack developer/UX & UI designer.
